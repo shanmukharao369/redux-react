dcd81b0e51e313ed2a138c21d97b66827facf313
shanmukharao369 <115773678+shanmukharao369@users.noreply.github.com>
2023-05-03 20:16:49 +0530

    Integrating Redux with react task-12

---
+++ src/store/index.js
@@ -0,0 +1,32 @@
+import {createStore} from 'redux';
+
+const counterReducer = (state = {counter:0 }, action)=>{
+    if (action.type === 'Increment') {
+        return{
+            counter: state.counter+1
+        };
+    }
+    if (action.type === 'Decrement') {
+        return{
+            counter: state.counter-1
+        }
+    }
+
+    if (action.type === 'INCREMENTBY2') {
+        return {
+            counter : state.counter+2
+        };
+    }
+
+    if (action.type === 'DECREMENTBY2') {
+        return {
+            counter : state.counter-2
+        };
+    }
+
+    return state;
+}
+
+const store = createStore(counterReducer);
+
+export default store;
