72031e8ed266ed409163825d24370e4b91c4dbd2
shanmukharao369 <115773678+shanmukharao369@users.noreply.github.com>
2023-05-04 11:13:00 +0530

    Subscribing and Dispatching Actions task-13

--- src/store/index.js
+++ src/store/index.js
@@ -1,26 +1,26 @@
 import {createStore} from 'redux';
 
 const counterReducer = (state = {counter:0 }, action)=>{
-    if (action.type === 'Increment') {
+    if (action.type === 'increment') {
         return{
             counter: state.counter+1
         };
     }
-    if (action.type === 'Decrement') {
+    if (action.type === 'decrement') {
         return{
             counter: state.counter-1
         }
     }
 
-    if (action.type === 'INCREMENTBY2') {
+    if (action.type === 'INCREMENTBY5') {
         return {
-            counter : state.counter+2
+            counter : state.counter+5
         };
     }
 
-    if (action.type === 'DECREMENTBY2') {
+    if (action.type === 'DECREMENTBY5') {
         return {
-            counter : state.counter-2
+            counter : state.counter-5
         };
     }
 
